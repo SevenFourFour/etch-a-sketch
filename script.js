a9cd92d4428eac20761a937b82ef1750af032238
Julian <code@sevenfourfour.net>
2022-07-21 15:56:15 -0500

    Add a 16x16 grid in the DOM

---
+++ script.js
@@ -0,0 +1,14 @@
+// Creates a 16x16 grid of square divs
+
+const pixel_container = document.getElementById('pixel-container')
+
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div')
+    pixel_container.appendChild(row)
+    for (let j = 0; j < 16; j++) {
+        const pixel = document.createElement('div')
+        pixel.className = 'pixel'
+        row.appendChild(pixel)
+    }
+}
